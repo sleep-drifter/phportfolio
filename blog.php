39b7d0ea1029b39469559923fbfcffc9d8c69ac2
Matt Wujek <mattwujek@gmail.com>
2014-08-01 14:38:44 -0700

    Removed Blog Description
    Removed Blog Description

--- blog.php
+++ blog.php
@@ -33,7 +33,6 @@
 
       <div class="col-sm-8 col-sm-offset-2 regular_text" style="font-size:16px;">
         
-            <p id="blog-header">As my work increasingly revolves around digital media, I find even more value in manually recording visual ideas. The best part about drawing is filling up an entire sketchbook with pictures, doodles, and notes. Looking back on my sketches allows me to revisit concepts and rearrange them with a fresh perspective. Sketchbooks are a vital component of my design process and my online sketchbook provides a space to share my thoughts, procedures, art, and other jiberish that shape my artistic identity.</p> 
 
 
      
